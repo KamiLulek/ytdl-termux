66eb17ab1f06e755a60f10ed506798d6a713ca97
KamiLulek <kamilgoto@gmail.com>
2026-04-25 01:22:18 +0200

    Update output.sh

--- output.sh
+++ output.sh
@@ -9,9 +9,10 @@ if [ -n "$FILE" ] && [ -f "$FILE" ]; then
     sleep 0
     echo "════════════════════════════════════════"
     echo "                 Pobrano                "
-    echo "════════════════════════════════════════"
+    echo ""
     echo "  $(basename "$FILE")"
     echo "════════════════════════════════════════"
+    echo ""
 
 else
     echo "Błąd: Nie znaleziono pliku: $1"
