b315d605511b19d6b76e7172df9c82b17902dc02
KamiLulek <kamilgoto@gmail.com>
2026-04-25 01:18:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,9 +17,11 @@ sleep 0
 
 # Pobranie ytdl.sh z GitHub raw
 curl -o ~/ytdl.sh https://raw.githubusercontent.com/KamiLulek/ytdl-termux/refs/heads/main/ytdl.sh
+curl -o ~/output.sh https://raw.githubusercontent.com/KamiLulek/ytdl-termux/refs/heads/main/output.sh
 
 # Nadanie uprawnień
 chmod +x ~/ytdl.sh
+chmod +x ~/output.sh
 
 sleep 0
 
@@ -36,9 +38,8 @@ source .bashrc
 sleep 0
 
 echo ""
-echo "---------------------------------"
-echo "      Instalacja zakończona      "
-echo "---------------------------------"
-echo "    Wpisz 'ytdl' żeby odpalić    "
-echo "---------------------------------"
+echo "════════════════════════════════════════"
+echo "          Instalacja zakończona         "
+echo "        Wpisz 'ytdl' żeby odpalić       "
+echo "════════════════════════════════════════"
 echo ""
