4d93f428218b584877ab8a60263d6a24e6808885
KamiLulek <kamilgoto@gmail.com>
2026-04-25 00:36:37 +0200

    Update output.sh

--- output.sh
+++ output.sh
@@ -4,12 +4,14 @@
 FILE="$1"
 
 if [ -n "$FILE" ] && [ -f "$FILE" ]; then
+    sleep 0
+    clear
+    sleep 0
     echo ""
     echo "════════════════════════════════════════"
     echo "  $(basename "$FILE")"
     echo "════════════════════════════════════════"
 
-    echo ""
 else
-    echo "⚠️ Błąd: Nie znaleziono pliku: $1"
+    echo "Błąd: Nie znaleziono pliku: $1"
 fi
